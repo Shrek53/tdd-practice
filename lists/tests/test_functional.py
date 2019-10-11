de3e5eca6936f7c422078b077fac32a35601213e
shrek53 <apon.ruet@gmail.com>
2019-10-12 03:35:39 +0600

    some code in attemp to pass the test_can_start_a_list_and_retrieve_it_later test

--- lists/tests/test_functional.py
+++ lists/tests/test_functional.py
@@ -21,7 +21,7 @@ class NewVisitorTest(unittest.TestCase):
         # She notices the page title and header mention to-do lists
         self.assertIn('To-do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('To-Do', header_text)
+        self.assertIn('To-do', header_text)
         # She is invited to enter a to-do item straight away
 
         inputbox = self.browser.find_element_by_id('id_new_item')
--- lists/views.py
+++ lists/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    return HttpResponse('<html><title>To-do list</title></html>')
+    return render(request, 'home.html')
