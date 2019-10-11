4df57510bf6eee921c9c7667787e74edd80bc483
shrek53 <apon.ruet@gmail.com>
2019-10-12 04:14:32 +0600

    changed tempate dir and added message to assert

--- lists/tests/test_unit.py
+++ lists/tests/test_unit.py
@@ -15,4 +15,4 @@ class HomePageTest(TestCase):
 
     def test_uses_home_template(self):
         response = self.client.get('/')
-        self.assertTemplateUsed(response, 'home.html')
+        self.assertTemplateUsed(response, 'lists/home.html')
