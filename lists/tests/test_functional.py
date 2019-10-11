a6076af79629096540f6b371007265951e919954
shrek53 <apon.ruet@gmail.com>
2019-10-12 03:45:46 +0600

    split one unit test into two

--- lists/tests/test_functional.py
+++ lists/tests/test_functional.py
@@ -18,14 +18,16 @@ class NewVisitorTest(unittest.TestCase):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
         self.browser.get('http://localhost:8000')
+
         # She notices the page title and header mention to-do lists
         self.assertIn('To-do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-do', header_text)
-        # She is invited to enter a to-do item straight away
 
+        # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+
         # She types "Buy peacock feathers" into a text box (Edith's hobby
         # is tying fly-fishing lures)
         inputbox.send_keys('Buy peacock feathers')
