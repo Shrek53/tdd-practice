bf51de9e47efc78a165a6e21055ddf6e0ac0a3b3
shrek53 <apon.ruet@gmail.com>
2019-10-12 02:48:30 +0600

    functional test for adding item in list

--- lists/tests/test_functional.py
+++ lists/tests/test_functional.py
@@ -1,5 +1,9 @@
+import time
+
 from selenium import webdriver
 import unittest
+
+from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -10,10 +14,36 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_root_url_has_To_do_in_it(self):
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
         self.browser.get('http://localhost:8000')
+        # She notices the page title and header mention to-do lists
         self.assertIn('To-do', self.browser.title)
-        # self.fail('Finish the test')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+        # She is invited to enter a to-do item straight away
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+        # She types "Buy peacock feathers" into a text box (Edith's hobby
+        # is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
+
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list table
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: But peacock feathers' for row in rows))
+
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edith is very
+        # methodical)
+
+        self.fail('Finish the test')
 
 
 if __name__ == '__main__':
--- lists/tests/test_unit.py
+++ lists/tests/test_unit.py
@@ -5,14 +5,15 @@ from lists.views import home_page
 
 
 class HomePageTest(TestCase):
+
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
-        responce = home_page(request)
-        html = responce.content.decode('utf-8')
+        response = home_page(request)
+        html = response.content.decode('utf-8')
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-do list</title>', html)
         self.assertTrue(html.endswith('</html>'))
